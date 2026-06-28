8b382be94ba2d19a9689bd46cceef2b8940fbe28
B-Elorchi <brahimelorchi1937@gmail.com>
2026-06-28 12:45:46 +0100

    fix: update paths for API and worker scripts in ecosystem configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,28 +1,28 @@
 module.exports = {
-  apps: [
-    {
-      name: "seeourbook-api",
-      script: "./venv/bin/uvicorn", 
-      args: "api.main:app --host 0.0.0.0 --port 8000",
-      interpreter: "none",
-      autorestart: true,
-      watch: false,
-      max_memory_restart: "1G",
-      env: {
-        NODE_ENV: "production",
-      }
-    },
-    {
-      name: "seeourbook-worker",
-      script: "./venv/bin/celery",
-      args: "-A api.worker worker --loglevel=info --concurrency=4",
-      interpreter: "none",
-      autorestart: true,
-      watch: false,
-      max_memory_restart: "2G",
-      env: {
-        NODE_ENV: "production",
-      }
-    }
-  ]
+	apps: [
+		{
+			name: "seeourbooks-api",
+			script: "/usr/bin/uvicorn",
+			args: "api.main:app --host 0.0.0.0 --port 8000",
+			interpreter: "none",
+			autorestart: true,
+			watch: false,
+			max_memory_restart: "1G",
+			env: {
+				NODE_ENV: "production",
+			},
+		},
+		{
+			name: "seeourbooks-worker",
+			script: "/usr/local/bin/celery",
+			args: "-A api.worker worker --loglevel=info --concurrency=4",
+			interpreter: "none",
+			autorestart: true,
+			watch: false,
+			max_memory_restart: "2G",
+			env: {
+				NODE_ENV: "production",
+			},
+		},
+	],
 };
